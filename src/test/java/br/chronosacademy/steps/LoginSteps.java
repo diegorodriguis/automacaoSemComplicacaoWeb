dac68749a160c26085beaa077f212b044075f5ec
Diego Rodrigues <diego.rodriguestti@gmail.com>
2024-08-29 21:35:02 -0300

    atualizando classes com steps de login

---
+++ src/test/java/br/chronosacademy/steps/LoginSteps.java
@@ -0,0 +1,59 @@
+package br.chronosacademy.steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class LoginSteps {
+
+    @Dado("que a modal esteja sendo exibida")
+    public void queAModalEstejaSendoExibida() {
+
+    }
+
+    @Quando("for realizado um clique fora da modal")
+    public void forRealizadoUmCliqueForaDaModal() {
+
+    }
+
+    @Entao("a modal deve ser fechada")
+    public void aModalDeveSerFechada() {
+
+    }
+
+    @Quando("for realizado um clique no icone")
+    public void forRealizadoUmCliqueNoIcone() {
+
+    }
+
+    @Quando("for realizado um clique no link Create new account")
+    public void forRealizadoUmCliqueNoLinkCreateNewAccount() {
+
+    }
+
+    @Entao("deve ser direcionado para tela de criacao de conta")
+    public void deveSerDirecionadoParaTelaDeCriacaoDeConta() {
+
+    }
+
+    @Quando("os campos de login sejam preechidos da seguinte forma")
+    public void osCamposDeLoginSejamPreechidosDaSeguinteForma() {
+
+    }
+
+    @Quando("for realizado o clique no botao sign in")
+    public void forRealizadoOCliqueNoBotaoSignIn() {
+
+
+    }
+
+    @Entao("deve ser logado no sistema")
+    public void deveSerLogadoNoSistema() {
+    }
+
+    @Entao("sistema deve exibir uma mensagem de erro")
+    public void sistemaDeveExibirUmaMensagemDeErro() {
+    }
+
+
+}
